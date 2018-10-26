31e5a1ac2ccc84f3ec048c2887b1acddd5551eec
hseniht <thinesh_kumar_mu@hotmail.com>
2018-10-26 11:23:34 +0800

    Removed tooltip ver 1

--- Polymeric-v3/js/custom.js
+++ Polymeric-v3/js/custom.js
@@ -93,10 +93,18 @@ function zoom (clickedId){
   }
 }
 
-// $(function () {
-//   $('[data-toggle="tooltip"]').tooltip()
-// });
-//
-// $('#tooltip1').tooltip({ boundary: 'scrollParent' });
-//
-// $('#tooltip1').tooltip('update');
+/*For Checkbox */
+
+/**
+ * Startup animation.
+ */
+var checkboxes = document.querySelectorAll('input[type="checkbox"]'),
+    setChecked = null;
+
+(setChecked = function(i) {
+  checkboxes[i].checked = true;
+
+  if (i < checkboxes.length - 1) {
+    setTimeout(setChecked, 180, i + 2);
+  }
+})(0);
